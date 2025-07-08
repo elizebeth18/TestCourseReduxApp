1522629fb651973c985d30138264c13759dceab4
Jilu Elizebeth <elizebeth18.jilu@gmail.com>
2025-07-08 10:14:48 +0530

    test viewEnquirySlice's extraReducer

--- src/__tests__/viewEnquirySlice.test.jsx
+++ src/__tests__/viewEnquirySlice.test.jsx
@@ -12,10 +12,44 @@ describe("ViewEnquries ExtraReducer", () => {
     it("should render pending", () => {
         const mockAction = {
             type: fetchEnquiresList.pending.type,
-            payload: {isLoading: true, enquiresList: []}
+            payload: { isLoading: true, enquiresList: [] }
         }
         const state = viewEnquiresReducer(initialState, mockAction);
         expect(state.isLoading).toBeTruthy();
         expect(state.enquiresList).toHaveLength(0);
-    })
+    });
+
+    it("should render fuilfilled", () => {
+        const mockAction = {
+            type: fetchEnquiresList.fulfilled.type,
+            payload: [
+                {
+                    "id": "2dae",
+                    "name": "Dasappan",
+                    "phoneNumber": "7412589630",
+                    "email": "jilu_das18@yahoo.co.in",
+                    "enquiryMessage": "what is C",
+                    "courseName": "Learn and Master C Programming For Absolute Beginners!",
+                    "errors": {
+                        "enquiryMessage": ""
+                    }
+                }]
+        }
+        const state = viewEnquiresReducer(initialState, mockAction);
+        expect(state.enquiresList).toEqual(mockAction.payload);
+        expect(state.isLoading).toBeFalsy();
+        expect(state.error).toBeNull();
+    });
+
+    it("should render rejected", () => {
+        const mockAction = {
+            type: fetchEnquiresList.rejected.type,
+            error: {
+                message: "Failed to load data"
+            }
+        }
+        const state = viewEnquiresReducer(initialState, mockAction);
+        expect(state.error).toEqual(mockAction.error.message);
+        expect(state.isLoading).toBeFalsy();
+    });
 })
